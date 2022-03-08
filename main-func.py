06d82f2e1c1c0a0d741aa0f622a2d8ba975d9a8b
Osama Abu Oun <o.abuoun@gmail.com>
2022-03-08 14:37:40 +0000

    added __main__ and default argument

--- main-func.py
+++ main-func.py
@@ -1,13 +1,15 @@
-def print_line(text):
+def print_line(text = "Default Value"):
     print("--------------- INPUT ---------------" + text)
 
 def read_name(name_type):
     while True:
+        print_line("Reading a Name")
         name_var = input(f"Please Enter The {name_type}:")
         if len(name_var.strip()) > 0:
-            break
+            #break
+            return name_var
 
-    return name_var
+    #return name_var
 
 '''
 def read_last_name():
@@ -20,41 +22,44 @@ def read_last_name():
 
 def read_age():
     while True:
+        print_line()
         age_str = input("Please Enter the Age:")
         if age_str.isdigit():
             age = int(age_str)
             if (age >= 18) and (age <= 100):
-                break
+                # break
+                return age
             else:
                 print("The age should be a digit between 18 and 100")
         else:
             print("The age should contain only digits")
-    return age
+    #return age
 
-#   Read how many employees you need to enter
-print_line("Employee Count")
-employee_count_str = input("Please enter how many employees you wish to enter:")
+if __name__ == '__main__':
+    #   Read how many employees you need to enter
+    print_line("Employee Count")
+    employee_count_str = input("Please enter how many employees you wish to enter:")
 
-employee_count = int(employee_count_str)
+    employee_count = int(employee_count_str)
 
-#   Loop through employees
-total_age = 0
-for employee_index in range(employee_count):
-    #   Read first name, last name and age
+    #   Loop through employees
+    total_age = 0
+    for employee_index in range(employee_count):
+        #   Read first name, last name and age
 
-    first_name = read_name("First Name")
+        first_name = read_name("First Name")
 
-    last_name = read_name("Last Name")
+        last_name = read_name("Last Name")
 
-    age = read_age()
+        age = read_age()
 
-    total_age = total_age + age
+        total_age = total_age + age
 
-    print(f"Employee Data: First Name: {first_name}, Last Name: {last_name}, Age: {age}")
-    print(f"Total Age = {total_age}")
+        print(f"Employee Data: First Name: {first_name}, Last Name: {last_name}, Age: {age}")
+        print(f"Total Age = {total_age}")
 
-if total_age > 500:
-    print("Warning: the total age for all employees is more than 500")
-else:
-    print("Warning: the total is less than 500")
+    if total_age > 500:
+        print("Warning: the total age for all employees is more than 500")
+    else:
+        print("Warning: the total is less than 500")
 
